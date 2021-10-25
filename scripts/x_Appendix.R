70041de2e44054c44a3ac3bf813c3f7a1d314d82
Josie Hughes <josie.hughes@canada.ca>
2021-10-25 13:38:19 -0400

    Begin work on example code for manuscript supplementary information

---
+++ scripts/x_Appendix.R
@@ -0,0 +1,37 @@
+library(LSTDConnect)
+library(pfocal)
+libraryr(raster)
+
+baseDir <- "C:/Users/HughesJo/Documents/InitialWork/Connectivity/ConnectivityMetricsForMonitoring/IALEiposter/KernelVisualizations"
+paID <- 4025
+inDir <- paste0(baseDir,"/pa",paID)
+H <- raster(paste0(inDir,"/newQuality.tif"))/100 
+#TO DO: add small example raster to LSTDConnect package
+
+#SAMC B high 5km example
+mortality <- (1-H)*(2/30-0.00001)+0.00001
+resistance <- (1-H)*(20-1)+1
+t <- 28
+samc_obj_custom <- samc_cache(resistance = as.matrix(resistance),
+                                           absorbtion = as.matrix(mortality),
+                                           kernel = 8)
+Bh5 <- samc_step(steps = t, cache = samc_obj_custom, population = as.matrix(H*100))
+
+Bh5 <- raster(Bh5$population[[1]], template = H)
+
+#Exponential 5km example
+displacement <- 5*1000/res(H)[1]  
+k <- exponentialKernel(displacement, negligible = 10^-6)
+E5 <- pfocal(as.matrix(H*100), kernel = k)
+E5 <- raster(E5, template = H)
+
+#Intactness 5km example
+displacement <- 5*1000/res(H)[1]  
+k <- exponentialKernel(displacement, negligible = 10^-6)
+occ_raster_z <- as.matrix(H*100) ^ 0.5
+I5 <- pfocal(occ_raster_z,na_flag = 0,kernel = k) * occ_raster_z
+I5 <- raster(I5,template=H)
+
+view = stack(H,Bh5,E5,I5)
+names(view)=c("H","Bh5","E5","I5")
+plot(view)
