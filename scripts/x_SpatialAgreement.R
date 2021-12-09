8e5e36855f0897dee35a64ade29484d5b3d0184e
Josie Hughes <josie.hughes@canada.ca>
2021-12-09 10:11:38 -0500

    more work on maps

--- scripts/x_SpatialAgreement.R
+++ scripts/x_SpatialAgreement.R
@@ -9,11 +9,16 @@ library(rasterVis)
 
 resultDir = "C:/Users/HughesJo/Documents/gitprojects/LSTD-Connectivity-Paper/outputs/rasters"
 
-makeMaps =T
-doStandardization=F
+doStandardization=T
+scale = 40
 
+################
 H = raster(paste0(resultDir,"/allCostsLayer.tif"))
 N = raster(paste0(resultDir,"/naturalCostsLayer.tif"))
+
+provs =  getData('GADM', country='CAN', level=1)
+provs= spTransform(provs,crs(H))
+countryR = fasterize(st_as_sf(provs),H)
 
 unique(H)
 
@@ -25,69 +30,91 @@ H = 1-H/1000
 N = 1-N/1000
 Anthro = (1-H)-(1-N)
 
-provs =  getData('GADM', country='CAN', level=1)
 provs = provs[is.element(provs@data$NAME_1,c("British Columbia","Alberta")),]
-provs= spTransform(provs,crs(Anthro))
-provs=st_as_sf(provs)
-provsR= fasterize(provs,Anthro)
+provsR= fasterize(st_as_sf(provs),Anthro)
 plot(provsR)
 
-if(makeMaps){
+layerSet = list.files(paste0(resultDir,"/Can_Cost"))
 
-  layerSet = list.files(paste0(resultDir,"/Can_Cost"))
-  
-  scales = 20#c(2,5,10,20,40)
-
-  for(s in scales){
-    print(s)
-    #s=20
-    selectTerm = as.character(s)
-    selectSet = layerSet[grepl(selectTerm,layerSet,fixed=T)]
-    if(s==2){
-      selectSet = selectSet[!grepl("20",selectSet,fixed=T)]
-    }
-    
-    view = stack(paste0(resultDir,"/Can_Cost/",selectSet))
-    if(doStandardization){
-      denom = stack(paste0(resultDir,"/Can_Cost_noH/",gsub(".tif","no_HF.tif",selectSet,fixed=T)))
-      for(i in 1:nlayers(view)){
-        view[[i]]=view[[i]]/denom[[i]]
-      }
-      rm(denom)
-    }  
-    
-    view <- crop(view, cropExtent)
-    
-    view[is.na(provsR)]=NA
-    view[N==0]=NA # only look at agreement/disagreement for places that aren't rock and ice
-    
-    quants = seq(0,1,length.out=101)#seq(0.25,1,length.out=4)
-    
-    qTab = quantile(view,probs=quants)
-    
-    cc = view
-    for(j in 1:nrow(qTab)){
-      cc[[j]] <- cut(view[[j]], breaks=unique(qTab[j,]))
-      
-    }
-    names(cc)=gsub(".tif","",selectSet,fixed=T)
-
-    plotStack = subset(cc,paste0(c("I","E","MH","BH"),s))
-    pdf(paste0(gsub("rasters","figures",resultDir,fixed=T),"/fig5Maps",selectTerm,"std",doStandardization,".pdf"),
-        width=8,height=3)
-    par(mar=c(0,0,0,0), oma=c(0,0,0,0))
-    levelplot(plotStack,xlab=NULL,ylab=NULL,scales=list(draw=FALSE),maxpixels = 2e5)
-    #plot(plotStack,axes=F,horizontal=T,nr=1)
-    dev.off()
-    
-    var = calc(cc, sd)
-    writeRaster(var,paste0(resultDir,"/derived/sd",selectTerm,"std",doStandardization,".tif"),overwrite=T)
-    
-    middle = calc(cc,mean)
-    writeRaster(middle,paste0(resultDir,"/derived/mean",selectTerm,"std",doStandardization,".tif"),overwrite=T)
-    rm(cc);rm(view)
-  }
+s=scale
+selectTerm = as.character(s)
+selectSet = layerSet[grepl(selectTerm,layerSet,fixed=T)]
+if(s==2){
+  selectSet = selectSet[!grepl("20",selectSet,fixed=T)]
 }
+
+view = stack(paste0(resultDir,"/Can_Cost/",selectSet))
+
+if(doStandardization){
+  denom = stack(paste0(resultDir,"/Can_Cost_noH/",gsub(".tif","no_HF.tif",selectSet,fixed=T)))
+  for(i in 1:nlayers(view)){
+    view[[i]]=view[[i]]/denom[[i]]
+  }
+  rm(denom)
+}  
+
+names(view)=gsub(".tif","",selectSet,fixed=T)
+plotStack = subset(view,paste0(c("I","ML","MH"),s))
+plotStack[[1]] = plotStack[[1]]*countryR
+plotStack[[2]] = plotStack[[2]]*countryR
+plotStack[[3]] = plotStack[[3]]*countryR
+names(plotStack)= paste0(c("I","ML","MH"),s)
+pdf(paste0(gsub("rasters","figures",resultDir,fixed=T),"/fig5MapsRawBig",selectTerm,"std",doStandardization,".pdf"),
+    width=8,height=3)
+par(mar=c(0,0,0,0), oma=c(0,0,0,0))
+levelplot(plotStack,xlab=NULL,ylab=NULL,scales=list(draw=FALSE),maxpixels = 2e5,col.regions=rev(terrain.colors(255)))
+dev.off()
+rm(plotStack)
+
+view <- crop(view, cropExtent)
+
+view[is.na(provsR)]=NA
+view[N==0]=NA # only look at agreement/disagreement for places that aren't rock and ice
+
+names(view)=gsub(".tif","",selectSet,fixed=T)
+
+plotStack = subset(view,paste0(c("I","ML","MH","BH"),s))
+pdf(paste0(gsub("rasters","figures",resultDir,fixed=T),"/fig5MapsRaw",selectTerm,"std",doStandardization,".pdf"),
+    width=8,height=3)
+par(mar=c(0,0,0,0), oma=c(0,0,0,0))
+levelplot(plotStack,xlab=NULL,ylab="unscaled metric values",scales=list(draw=FALSE),
+          maxpixels = 2e5,col.regions=rev(terrain.colors(255)))
+#plot(plotStack,axes=F,horizontal=T,nr=1)
+dev.off()
+rm(plotStack)
+
+quants = seq(0,1,length.out=101)#seq(0.25,1,length.out=4)
+
+qTab = quantile(view,probs=quants)
+
+cc = view
+for(j in 1:nrow(qTab)){
+  cc[[j]] <- cut(view[[j]], breaks=unique(qTab[j,]))
+  
+}
+names(cc)=gsub(".tif","",selectSet,fixed=T)
+
+plotStack = subset(cc,paste0(c("I","ML","MH","BH"),s))
+pdf(paste0(gsub("rasters","figures",resultDir,fixed=T),"/fig5Maps",selectTerm,"std",doStandardization,".pdf"),
+    width=8,height=3)
+if(doStandardization){
+  xlab = "percentiles scaled"
+}else{
+  xlab="percentiles unscaled"
+}
+par(mar=c(0,0,0,0), oma=c(0,0,0,0))
+levelplot(plotStack,ylab=xlab,xlab=NULL,scales=list(draw=FALSE),maxpixels = 2e5,
+          col.regions=rev(terrain.colors(255)))
+#plot(plotStack,axes=F,horizontal=T,nr=1)
+dev.off()
+
+var = calc(cc, sd)
+writeRaster(var,paste0(resultDir,"/derived/sd",selectTerm,"std",doStandardization,".tif"),overwrite=T)
+
+middle = calc(cc,mean)
+writeRaster(middle,paste0(resultDir,"/derived/mean",selectTerm,"std",doStandardization,".tif"),overwrite=T)
+rm(cc);rm(view)
+removeTmpFiles(h=0.1)
 
 outSet =   list.files(paste0(resultDir,"/derived"))
 outSet=outSet[grepl("20",outSet,fixed=T)]
@@ -109,51 +136,7 @@ pdf(paste0(gsub("rasters","figures",resultDir,fixed=T),"/fig4Maps20std",doStanda
 par(mar=c(0,0,0,0), oma=c(0,0,0,0))
 #levelplot(cStack,xlab=NULL,ylab=NULL,scales=list(draw=FALSE),maxpixels = 2e5)
 plot(cStack,axes=F,horizontal=T,nr=1)
-plot(provs,add=T)
 dev.off()
 
+removeTmpFiles(h=0.1)
 
-
-
-if(0){
-  option="percentile"
-  
-  for(i in 1:length(quants)){
-    #i=1
-    outStack=view
-    cLevels = qTab[,i]
-    for(j in 1:length(cLevels)){
-      #j=1
-      
-      
-      outStack[[j]][!is.na(view[[j]])]=1
-      if(option=="highest"){
-        outStack[[j]][view[[j]]< cLevels[j]]=0
-        cName=paste(option, (1-quants[i])*100,"percent")
-      }
-      if(option=="lowest"){
-        outStack[[j]][view[[j]]>= cLevels[j]]=0
-        cName=paste(option, quants[i]*100,"percent")
-      }
-      if(option=="percentile"){
-        outStack[[j]][view[[j]]>= cLevels[j]]=0
-        if(i>1){
-          outStack[[j]][view[[j]]< oLevels[j]]=0
-        }
-        cName=paste(quants[i]*100,"percentile")
-      }
-    }
-    oLevels=cLevels
-    
-    rs1 <- calc(outStack, sum)
-    names(rs1)=cName
-    
-    if(i==1){
-      res = stack(rs1)
-    }else{
-      res = addLayer(res,rs1)
-    }
-  }
-  
-  plot(res)
-}
