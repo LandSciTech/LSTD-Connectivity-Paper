3f980a3a7a62f4e1fb62bdbc6b3f2a3e6d3d8e1a
see24 <sarahendicott4@gmail.com>
2024-02-13 11:32:51 -0500

    trying using posit PPM

--- make.R
+++ make.R
@@ -1,10 +1,12 @@
 
 # I copied these from the rocker/r-bspm Dockerfile because it sets them in the
 # RProfile but it wasn't working
-bspm::enable()
-options(pkgType="binary", install.packages.check.source = "no")
+# bspm::enable()
+# options(pkgType="binary", install.packages.check.source = "no")
 
-utils::install.packages("remotes", dependencies = TRUE)
+r = getOption("repos")
+r["CRAN"] = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"
+options(repos = r)
 
 # restore required packages with renv
 install.packages("renv")
