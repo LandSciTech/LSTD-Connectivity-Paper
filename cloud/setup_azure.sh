50a28062a5d3e5ca5aeb9a9a9b629a51155d87df
see24 <sarahendicott4@gmail.com>
2024-02-13 09:55:11 -0500

    getting set up for cloud

--- cloud/setup_azure.sh
+++ cloud/setup_azure.sh
@@ -6,27 +6,35 @@ az login
 az batch account login -g EcDc-WLS-rg -n ecdcwlsbatch
 az batch pool list
 
-
-#### Move files to container ##############
-end=`date -u -d "20 days" '+%Y-%m-%dT%H:%MZ'`
-sastoken=`az storage container generate-sas --account-name ecdcwls --expiry $end --name sendicott --permissions racwdli -o tsv --auth-mode login --as-user`
-
-
-sasurl=https://ecdcwls.blob.core.windows.net/sendicott/?$sastoken
+#### Set ids and names ##############
+subnetid=$(az network vnet subnet list --resource-group EcDc-WLS-rg --vnet-name EcDc-WLS-vnet \
+--query "[?name=='EcDc-WLS_compute-cluster-snet'].id" --output tsv)
 
 jobName="sendicott_job_connect"
 
 poolName="sendicott_connectivity"
 
-az storage copy -d $sasurl -s cloud/run_connect.sh
-az storage copy -d $sasurl -s ../LSTD-Connectivity-Paper --recursive
+#### Move files to container ##############
+end=`date -u -d "7 days" '+%Y-%m-%dT%H:%MZ'`
+sastoken=`az storage container generate-sas --account-name ecdcwls --expiry $end --name sendicott --permissions racwdli -o tsv --auth-mode login --as-user`
 
-# TODO: modify scripts to add subnet id and other secrets from here
-# update placeholders in jsons
-sed 's/<sastoken>/'${sastoken//&/\\&}'/g' cloud/task_connect.json > cloud/task_to_use.json
+sasurl=https://ecdcwls.blob.core.windows.net/sendicott/?$sastoken
+
+az storage copy -d $sasurl -s cloud/run_connect.sh
+az storage copy -d $sasurl -s make.R
+az storage copy -d $sasurl -s data --recursive
+az storage copy -d $sasurl -s scripts --recursive
+az storage copy -d $sasurl -s outputs --recursive
 
 #### Create pool, job, tasks ##########################
 # AWS machine was m5a.4xlarge with 16 vCPU and 64 gb RAM
+
+# Replace placeholders for secrets in json file
+# Using , as delimter for sed to work with url
+sed 's,<SASURL>,'${sasurl//&/\\&}',g' cloud/task_connect.json > cloud/task_to_use.json
+
+sed 's,<subnetId>,'${subnetid//&/\\&}',g' cloud/pool_connect.json\
+| sed 's,<id>,'${poolName}',g'> cloud/pool_to_use.json
 
 az batch pool create --json-file cloud/pool_connect.json
 az batch job create --pool-id $poolName --id $jobName
@@ -53,7 +61,7 @@ az batch job task-counts show --job-id $jobName
 
 # Check what results have been added to the storage container
 az storage blob list -c sendicott --account-name ecdcwls --sas-token $sastoken \
---query "[].{name:name}" --prefix $setName --output yaml
+--query "[].{name:name}" --output yaml
 
 #### Download results and remove from storage ################################
 az storage copy -s https://ecdcwls.blob.core.windows.net/sendicott/$setName/?$sastoken \
