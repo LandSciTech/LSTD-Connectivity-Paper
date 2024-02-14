119c86a6bb9f6bcc2e4f3a2b750b643cf8ee3702
see24 <sarahendicott4@gmail.com>
2024-02-14 09:44:57 -0500

    make work with bspm

--- make.R
+++ make.R
@@ -1,18 +1,17 @@
 
-# I copied these from the rocker/r-bspm Dockerfile because it sets them in the
-# RProfile but it wasn't working
-# bspm::enable()
-# options(pkgType="binary", install.packages.check.source = "no")
+# I copied these from the rocker/r2u instructions. It seems the Rprofile.site
+# from the docker image is not being used so need to set here 
+options(bspm.version.check=FALSE)
+bspm::enable()
 
-
-
-# restore required packages with renv
+# install required packages based on list in DESCRIPTION
 install.packages("remotes")
 remotes::install_deps()
 
-source("scripts/1_prepare_data.R")
+# source("scripts/1_prepare_data.R")
 source("scripts/2_connectivity_analysis.R")
 
+# Used to get list of required packages for just the analysis scripts
 # deps1 <- rcompendium:::get_deps_extra("scripts/scripts2") |>
 #   stringr::str_extract("(.*)::", group = 1) |> unique()
 # 
@@ -20,3 +19,4 @@ source("scripts/2_connectivity_analysis.R")
 #   stringr::str_subset("::", negate = T) |> unique()
 # 
 # dplyr::union(deps1, deps2)|> na.omit() |> sort() |> paste0(collapse = ",\n")|> cat()
+
